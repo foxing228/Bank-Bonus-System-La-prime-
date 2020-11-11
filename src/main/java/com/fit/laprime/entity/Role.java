8da10a96f972c24faa9f60550a272896cbcbf0b1
Dmytro Prokhorchuk <g.prokhorchuk@gmail.com>
2020-11-11 21:26:16 +0200

    Add starter classes, database tables

---
+++ src/main/java/com/fit/laprime/entity/Role.java
@@ -0,0 +1,30 @@
+package com.fit.laprime.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/test/java/com/fit/laprime/LaprimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.laprime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaprimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
